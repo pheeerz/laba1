a9a9ceb51fe95dce045054079adb42ff352fe779
Султан <a@a.a>
2024-10-16 10:32:21 +0700

    Переписана ф-ия f

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int f(List<float> list,int from, int to) 
+            {
+                int cnt = 0;
+                foreach (float item in list)
+                {
+                    if (item >= from && item <= to)
+                    {
+                        cnt++;
+                    }
+                    else if(item <= from && item >= to)
+                    {
+                        cnt++;
+                    }
+                }
+                return cnt;
+            }
+            Subject ToObj(string str)
+            {
+                str = str.Trim();
+                List<string> items = str.Split('"').ToList();
+                //if (items.Count == 3) 
+                //{
+                    List<string> items2 = items[0].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                    string name = items[1].Trim();
+                    DateTime date = DateTime.Parse(items2[0].Trim()).Date;
+                    TimeSpan time = DateTime.Parse(items2[1].Trim()).TimeOfDay;
+                    return new Subject(date, time, name);
+                //}
+                //else
+                //{
+                //    List<string> items2 = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                //    string name = items2[2].Trim();
+                //    DateTime date = DateTime.Parse(items2[0].Trim()).Date;
+                //    TimeSpan time = DateTime.Parse(items2[1].Trim()).TimeOfDay;
+                //    return new Subject(date, time, name);
+                //}
+            }
+            string test = "17.02.2022             20:30:48                \"Джордж Хендрикс\"";
+            Subject testObj = ToObj(test);
+            Console.WriteLine($"Дата: {testObj.Date.ToString("dd.MM.yyyy")}\nВремя: {testObj.Time}\nИмя:{testObj.Name}");
+            Console.ReadKey();
+        }
+    }
+}
