a101bf2e6f5dcbfe326429971e4eb372a2b74473
Султан <a@a.a>
2024-10-16 10:38:17 +0700

    1st commit

---
+++ Peace/Peace/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int f(List<float> arr, int from, int to)
+            {
+                int cnt = 0;
+                for (int j = 0; j < arr.Count(); j++)
+                {
+                    if (from > to) { 
+                        if (arr[j] >= from && arr[j] <= to)
+                        {
+                            cnt++;
+                        }
+                    }
+                    else
+                    {
+                        if (arr[j] <= from && arr[j] >= to)
+                        {
+                            cnt++;
+                        }
+                    }
+                }
+                return cnt;
+            }
+            Subject ToObj(string str)
+            {
+                str = str.Trim();
+                List<string> items = str.Split('"').ToList();
+                if (items.Count == 3) 
+                {
+                    List<string> items2 = items[0].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                    string name = items[1].Trim();
+                    DateTime date = DateTime.Parse(items2[0].Trim()).Date;
+                    TimeSpan time = DateTime.Parse(items2[1].Trim()).TimeOfDay;
+                    return new Subject(date, time, name);
+                }
+                else
+                {
+                    List<string> items2 = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                    string name = items2[2].Trim();
+                    DateTime date = DateTime.Parse(items2[0].Trim()).Date;
+                    TimeSpan time = DateTime.Parse(items2[1].Trim()).TimeOfDay;
+                    return new Subject(date, time, name);
+                }
+            }
+            string test = "17.02.2022             20:30:48                \"Джордж Хендрикс\"";
+            Subject testObj = ToObj(test);
+            Console.WriteLine($"Дата: {testObj.Date.ToString("dd.MM.yyyy")}\nВремя: {testObj.Time}\nИмя:{testObj.Name}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Peace/Subject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peace
+{
+    internal class Subject
+    {
+        public DateTime Date;
+        public TimeSpan Time;
+        public string Name;
+        public Subject(DateTime date, TimeSpan time, string name) 
+        {
+            Date = date;
+            Time = time;
+            Name = name;
+        }
+    }
+}
